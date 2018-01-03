19c7606d404b211c33fc0eb299dafce576f842f2
Ihor Vitruk <Ihor_Vitruk@epam.com>
2018-01-03 19:03:14 +0200

    andori darchitecture lifecycle + databinding gradle integration

--- splash/src/main/java/com/ihorvitruk/telegramclient/splash/SplashViewModel.java
+++
@@ -1,5 +0,0 @@
-package com.ihorvitruk.telegramclient.splash;
-
-public class SplashViewModel {
-
-}
---
+++ splash/src/main/java/com/ihorvitruk/telegramclient/splash/SplashViewModel.kt
@@ -0,0 +1,8 @@
+package com.ihorvitruk.telegramclient.splash
+
+import android.arch.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+
+}
+
