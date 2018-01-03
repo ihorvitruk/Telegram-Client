e0236e72396b83f78d77e1631d69bcf4b0bc40cd
Ihor Vitruk <Ihor_Vitruk@epam.com>
2018-01-03 20:00:07 +0200

    success run andori arch components with data binding on canary 6 with mutablelivedata

--- splash/src/main/java/com/ihorvitruk/telegramclient/splash/SplashActivity.kt
+++ splash/src/main/java/com/ihorvitruk/telegramclient/splash/SplashActivity.kt
@@ -1,12 +1,19 @@
 package com.ihorvitruk.telegramclient.splash
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.ihorvitruk.telegramclient.splash.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        val splashViewModel = SplashViewModel()
+        val binding = DataBindingUtil.setContentView<ActivitySplashBinding>(this, R.layout.activity_splash)
+        binding?.viewModel = splashViewModel
+        binding?.setLifecycleOwner(this)
+
+        splashViewModel.onCreate()
     }
 }
--- splash/src/main/java/com/ihorvitruk/telegramclient/splash/SplashViewModel.kt
+++ splash/src/main/java/com/ihorvitruk/telegramclient/splash/SplashViewModel.kt
@@ -1,8 +1,12 @@
 package com.ihorvitruk.telegramclient.splash
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 class SplashViewModel : ViewModel() {
 
+    val title = MutableLiveData<String>()
+
+    fun onCreate() = title.postValue("Hello")
 }
 
