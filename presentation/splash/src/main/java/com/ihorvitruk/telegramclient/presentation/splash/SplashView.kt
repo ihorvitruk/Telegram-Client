b316a6e27d1cc6362a990f53b2494aa394df7f83
Ihor Vitruk <Ihor_Vitruk@epam.com>
2018-01-30 16:55:47 +0200

    BindingAdapter override example. That is AWASOME!!!

--- presentation/splash/src/main/java/com/ihorvitruk/telegramclient/presentation/splash/SplashView.kt
+++ presentation/splash/src/main/java/com/ihorvitruk/telegramclient/presentation/splash/SplashView.kt
@@ -1,7 +1,11 @@
 package com.ihorvitruk.telegramclient.presentation.splash
 
 import android.content.Context
+import android.databinding.BindingAdapter
+import android.text.TextUtils
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
 import com.ihorvitruk.telegramclient.presentation.base.BaseView
 import com.ihorvitruk.telegramclient.presentation.splash.databinding.SplashViewBinding
 
@@ -13,5 +17,16 @@ class SplashView(context: Context?, viewModel: SplashViewModel)
 
     override fun initViewModel(binding: SplashViewBinding?, viewModel: SplashViewModel) {
         binding?.viewModel = viewModel
+    }
+
+    //Override example! That is AWESOME!
+    companion object {
+        @JvmStatic
+        @BindingAdapter("showError")
+        fun showError(container: View, errorText: String?) {
+            if (!TextUtils.isEmpty(errorText)) {
+                Toast.makeText(container.context, "Changed: " + errorText, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
